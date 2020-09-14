c295db3f68fdff0432c3f56d16151b4e8e77cef9
arvicrin <1231@holbertonschool.com>
2020-09-14 23:24:57 +0000

    asm_putc ok

--- 0x05-libasm/6-putc.asm
+++ 0x05-libasm/6-putc.asm
@@ -9,5 +9,19 @@ section .text
 
 ; string length funciton
 asm_putc:
-        nop
-        ret
+        push rbp		; Setup stack frame
+	mov rbp, rsp		; Those two lines are equivalent to 'enter'
+
+	push rdi		; store the arg1 value
+
+	mov rax, 1		; Write syscall
+	mov rdi, 1		; write to stdout
+	mov rsi, rsp		; Address of the character to be printed (Address of the stack)
+	mov rdx, 1		; Write 1 byte
+	syscall			; call the syscall
+
+	pop rdi			; set back the previos value
+	mov rax, 1		; return value printd
+	mov rsp, rbp		; Restore previous stack frame
+	pop rbp			; Those two lines are equivalent to 'leave'
+        ret			; return
