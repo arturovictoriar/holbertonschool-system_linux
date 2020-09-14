50b603169dcea159d48ca2cc3cb89b1524be4d28
arvicrin <1231@holbertonschool.com>
2020-09-14 20:20:46 +0000

    0-4 taks ok, the other is just the file. Add Readme and header

---
+++ 0x05-libasm/libasm.h
@@ -0,0 +1,11 @@
+#ifndef ASM_H
+#define ASM_H
+
+int asm_strlen(char *a);
+int asm_strcmp(const char *s1, const char *s2);
+int asm_strncmp(const char *s1, const char *s2, size_t n);
+char *asm_strchr(const char *s, int c);
+char *asm_strstr(const char *haystack, const char *needle);
+#endif
+
+
