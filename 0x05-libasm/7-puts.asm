1daa9a596fe45157c0e8968b8b4204ce4bfbce54
arvicrin <1231@holbertonschool.com>
2020-09-15 00:23:27 +0000

    fix asm_puts output

--- 0x05-libasm/7-puts.asm
+++ 0x05-libasm/7-puts.asm
@@ -2,18 +2,21 @@ BITS 64
 
 section .data
 
+str_c dq 0
+
 section .bss
 
 section .text
         global asm_puts
+	extern asm_strlen
 
 ; string length funciton
 asm_puts:
-        push rbp                ; Setup stack frame
+	push rbp                ; Setup stack frame
 	mov rbp, rsp            ; Those two lines are equivalent to 'enter'
 
 	push rbx		; save rbx value
-	push rdi                ; store the arg1 value
+	mov [str_c], rdi	; store the arg1 value
 
 	mov rax, 0		; set 0 the return value
 	call asm_strlen		; call the function asm_strlen
@@ -21,13 +24,12 @@ asm_puts:
 
 	mov rax, 1              ; Write syscall
 	mov rdi, 1              ; write to stdout
-	mov rsi, rsp            ; Address of the character to be printed (Address of the stack)
+	mov rsi, [str_c]        ; Address of the character to be printed (Address of the stack)
 	mov rdx, rbx		; Write strlen bytes
 	syscall                 ; call the syscall
 
 	mov rax, rbx            ; return value printd
 
-	pop rdi                 ; set back the previos value
 	pop rbx			; set back the previos value
 	mov rsp, rbp            ; Restore previous stack frame
 	pop rbp                 ; Those two lines are equivalent to 'leave'
