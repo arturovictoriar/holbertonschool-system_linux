c4097fd86bc700a2d4a156ccfe5b98b4ca4c3380
arvicrin <1231@holbertonschool.com>
2020-11-25 22:19:45 +0000

    Print parameters

--- 0x09-strace/3-ptrace.c
+++ 0x09-strace/3-ptrace.c
@@ -1,5 +1,35 @@
 #include "syscalls.h"
 
+
+/**
+* print_parameters - print the parametres of a given syscall
+* @regs: struct with the register syscall
+* Return: nothing
+*/
+void print_parameters(struct user_regs_struct regs)
+{
+	unsigned long para[6] = {0};
+	size_t nb_params, i = 0;
+
+	para[0] = (unsigned long) regs.rdi;
+	para[1] = (unsigned long) regs.rsi;
+	para[2] = (unsigned long) regs.rdx;
+	para[3] = (unsigned long) regs.rcx;
+	para[4] = (unsigned long) regs.r8;
+	para[5] = (unsigned long) regs.r9;
+
+	nb_params = syscalls_64_g[(unsigned long)regs.orig_rax].nb_params;
+
+	while (i < nb_params)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%#lx", para[i]);
+		i++;
+	}
+}
+
+
 /**
  * main - use ptrace for getting the syscall name and return value
  * @ac: number of arguments
@@ -15,7 +45,7 @@ int main(int ac, char **av, char **en)
 
 	if (ac < 2)
 	{
-		printf("./strace_2 command [args...]\n");
+		printf("./strace_3 command [args...]\n");
 		return (1);
 	}
 	setbuf(stdout, NULL);
@@ -37,13 +67,14 @@ int main(int ac, char **av, char **en)
 			memset(&regs, 0, sizeof(regs));
 			ptrace(PTRACE_GETREGS, child, NULL, &regs);
 			if (flag)
-				(printf(" = %#lx\n", (unsigned long) regs.rax), flag = 0);
+				(printf(") = %#lx\n", (unsigned long) regs.rax), flag = 0);
 			if (WSTOPSIG(status) == SIGTRAP && (long) regs.rax == -38)
-				(printf("%s",
-					(char *) syscalls_64_g[(unsigned long)regs.orig_rax].name), flag = 1);
+				(printf("%s(",
+					(char *) syscalls_64_g[(unsigned long)regs.orig_rax].name),
+				 print_parameters(regs), flag = 1);
 			ptrace(PTRACE_SYSCALL, child, NULL, NULL);
 		}
-		printf(" = ?\n");
+		printf(") = ?\n");
 	}
 	return (0);
 }
